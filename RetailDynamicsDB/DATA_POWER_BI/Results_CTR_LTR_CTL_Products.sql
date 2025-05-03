b6e9a029c2c5562c9b9b8ad8138a1ba38ef7a40c
César Gabriel Ruíz Muñoz <129628630+cesaruiz042804@users.noreply.github.com>
2025-05-03 00:08:08 -0400

    Update Results_CTR_LTR_CTL_Products.sql

--- RetailDynamicsDB/DATA_POWER_BI/Results_CTR_LTR_CTL_Products.sql
+++ RetailDynamicsDB/DATA_POWER_BI/Results_CTR_LTR_CTL_Products.sql
@@ -7,29 +7,28 @@ SELECT
 	Click,
 	Likes,
 	((CAST(Click AS FLOAT) / Views) * 100) AS CTR, -- TASA DE CLICK
-	((CAST(Likes AS FLOAT) / Views) * 100) AS VTL, -- TASA DE CONVERSIÓN DE VISTAS A LIKES
+	((CAST(Likes AS FLOAT) / Views) * 100) AS VTL, -- TASA DE CONVERSIÃ“N DE VISTAS A LIKES
 	((CAST(Click AS FLOAT) / NULLIF(Likes, 0)) * 100) AS CTL, -- TASA DE CLICKS POR LIKES
 	((CAST(Likes AS FLOAT) / NULLIF(Click, 0)) * 100) AS LTC,  -- TASA DE LIKES POR CLICK
 	ReviewDate
 FROM ProductReviews;
+
 -- SE UTILIZA CAST PARA CONVERTIR LA COLUMNA EN OTRO TIPO DE DATO
 
 /*
-CTR (Tasa de Clics): Como ya habíamos discutido, esta métrica te indica el porcentaje de 
-veces que se hizo clic en un producto después de ser visto. Un valor más alto sugiere 
-que la presentación del producto es atractiva para los usuarios.
+CTR (Tasa de Clics): Como ya habÃ­amos discutido, esta mÃ©trica te indica el porcentaje de 
+veces que se hizo clic en un producto despuÃ©s de ser visto. Un valor mÃ¡s alto sugiere 
+que la presentaciÃ³n del producto es atractiva para los usuarios.
 
-LTR (Tasa de Likes por Vista): Esta métrica calcula el porcentaje de veces que un producto 
-recibió un "me gusta" después de ser visto. Un valor alto indica que el producto genera una 
-reacción positiva entre quienes lo visualizan.
+LTR (Tasa de Likes por Vista): Esta mÃ©trica calcula el porcentaje de veces que un producto 
+recibiÃ³ un "me gusta" despuÃ©s de ser visto. Un valor alto indica que el producto genera una 
+reacciÃ³n positiva entre quienes lo visualizan.
 
-CTL (Tasa de Clicks por Like): Esta métrica muestra el número de clics que recibe un producto 
-por cada "me gusta". Un valor más alto podría sugerir que las personas que interactúan 
-inicialmente con el producto (a través de un clic) son menos propensas a dejar un "me gusta", o viceversa. 
-Es una métrica menos común pero podría revelar patrones interesantes en la interacción.
+CTL (Tasa de Clicks por Like): Esta mÃ©trica muestra el nÃºmero de clics que recibe un producto 
+por cada "me gusta". Un valor mÃ¡s alto podrÃ­a sugerir que las personas que interactÃºan 
+inicialmente con el producto (a travÃ©s de un clic) son menos propensas a dejar un "me gusta", o viceversa. 
+Es una mÃ©trica menos comÃºn pero podrÃ­a revelar patrones interesantes en la interacciÃ³n.
 
 */
 
-
-SELECT SUM(Click) AS Clicks FROM ProductReviews
 
